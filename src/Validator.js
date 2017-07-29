4dc3dd6f6502b0d07c6d5149f37fefeff3be2400
Gnucki <tprelot@gmail.com>
2017-07-29 19:21:30 +0200

    Upgrade coding convetions

--- src/Validator.js
+++ src/Validator.js
@@ -2,29 +2,27 @@
 
 const validationFunction = Symbol('validationFunction');
 
+/**
+ * @class Validator
+ */
 module.exports = class Validator {
   /**
    * Validation function.
-   *
    * @type {function}
-   *
-   * @throws {TypeError} If given value is not a function.
+   * @throws {TypeError} On unexpected value.
    */
   set validationFunction(value) {
     if (typeof value !== 'function') {
-      throw new TypeError(`Validation function must be a function, got a ${typeof value} instead`);
+      throw new TypeError(`Expected a function, got ${typeof value} instead`);
     }
     this[validationFunction] = value;
   }
 
   /**
    * Validate a value.
-   *
    * @param {*} value - The value.
-   *
    * @returns {*} A validated value.
-   *
-   * @throws {Error} If validation failed.
+   * @throws {Error} On validation failure.
    */
   validate(value) {
     return this[validationFunction](value);
--- src/ValidatorBuilder.js
+++ src/ValidatorBuilder.js
@@ -2,26 +2,25 @@
 
 const constructor = Symbol('constructor');
 
+/**
+ * @class ValidatorBuilder
+ */
 module.exports = class ValidatorBuilder {
   /**
    * Class constructor.
-   *
    * @type {function}
-   *
-   * @throws {TypeError} If given value is not a function.
+   * @throws {TypeError} On unexpected value.
    */
   set classConstructor(value) {
     if (typeof value !== 'function') {
-      throw new TypeError(`Class constructor must be a class constructor function, got a ${typeof value} instead`);
+      throw new TypeError(`Expected a class constructor function, got ${typeof value} instead`);
     }
     this[constructor] = value;
   }
 
   /**
    * Build a validator.
-   *
    * @param {function} validate - The validation function.
-   *
    * @returns {Validator} A validator.
    */
   build(validate) {
